d06329dac81144cf69739e727b45426ed39333b1
FaRnOoShKaRiMi <farnoosh.karimi99@gmail.com>
2025-02-22 12:11:41 +0330

    data saved in local storage

--- script/productScript.js
+++ script/productScript.js
@@ -1,16 +1,26 @@
 
 const API_URL = "https://fakestoreapi.com/products"
-let product = []
+let products = []
 let selectedCategory = null
 
 const fetchProduct = async () => {
 
-    try { }
+    try {
+        const cachedData = localStorage.getItem('products')
+        if (cachedData) {
+            products.JSON.parse(cachedData)
+        }
+
+        const response = await fetch(API_URL);
+        const newProducts = await response.json();
+        localStorage.setItem("products", JSON.stringify(newProducts));
+        products = newProducts;
+        displayProducts(products)
+    }
+
     catch (error) {
         console.log(error)
     }
 
-    finally {
-        setLoading(false)
-    }
+    
 }
