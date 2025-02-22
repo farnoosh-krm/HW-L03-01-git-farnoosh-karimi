92ddadce452b5c6478e1fa7d39a494da92a6b9ea
FaRnOoShKaRiMi <farnoosh.karimi99@gmail.com>
2025-02-22 12:45:05 +0330

    show products added

--- script/productScript.js
+++ script/productScript.js
@@ -6,7 +6,7 @@ let selectedCategory = null
 const fetchProduct = async () => {
 
     try {
-        
+
         const cachedData = localStorage.getItem('products')
 
         if (cachedData) {
@@ -16,8 +16,8 @@ const fetchProduct = async () => {
 
         const response = await fetch(API_URL);
         const newProducts = await response.json();
-        localStorage.setItem("products", JSON.stringify(newProducts));
-        products = newProducts;
+        localStorage.setItem("products", JSON.stringify(newProducts))
+        products = newProducts
         displayProducts(products)
     }
 
@@ -25,5 +25,30 @@ const fetchProduct = async () => {
         console.log(error)
     }
 
-    
+
 }
+
+
+const displayProducts = (items) => {
+
+    const productList = document.getElementById("productList")
+    productList.innerHTML = ""
+
+    if (items.length === 0) {
+        productList.innerHTML = " <p>No Products Found</p>"
+        return
+    }
+    
+    items.forEach(product => {
+        const productCard = document.createElement("div")
+        productCard.className = "product-card"
+        productCard.innerHTML = `
+        <img src="${product.image}" alt="${product.title}"><h4>${product.title}</h4>
+        <p>Price: ${product.price}</p>
+        <button onclick="showsimilarProducts(\`${product.category}\`)">Similar Products</button>
+        `
+        productList.appendChild(productCard)
+    })
+}
+
+document.addEventListener("DOMContentLoaded", fetchProduct)
