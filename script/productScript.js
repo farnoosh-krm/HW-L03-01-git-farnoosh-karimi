1b092023f5f579851c84334ced3acee40ade0aa5
FaRnOoShKaRiMi <farnoosh.karimi99@gmail.com>
2025-02-22 11:59:49 +0330

    products fetched from fake API

--- script/productScript.js
+++ script/productScript.js
@@ -2,3 +2,15 @@
 const API_URL = "https://fakestoreapi.com/products"
 let product = []
 let selectedCategory = null
+
+const fetchProduct = async () => {
+
+    try { }
+    catch (error) {
+        console.log(error)
+    }
+
+    finally {
+        setLoading(false)
+    }
+}
